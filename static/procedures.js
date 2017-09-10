8a3a35bad54bc84335de3654c61792bf405e6b14
Luke Sweeney <luke@thesweeneys.org>
2017-09-10 10:36:51 -0500

    Stops using cdns and some small improvments

--- static/procedures.js
+++ static/procedures.js
@@ -1,24 +1,36 @@
 var procedureBuilder = new Vue({
   el: '#procedure-builder',
   data: {
-    relays: {
+    items: {
       binaries: [
         {
           "type": "binary",
           "name": "hlt",
           "state": 0,
+          "stateVerbage": {
+            1: 'open',
+            0: 'closed'
+          },
           "prettyName": "HLT Valve"
         },
         {
           "type": "binary",
           "name": "pump",
           "state": 0,
+          "stateVerbage": {
+            1: 'on',
+            0: 'off'
+          },
           "prettyName": "Pump"
         },
         {
           "type": "binary",
           "name": "rims",
           "state": 0,
+          "stateVerbage": {
+            1: 'on',
+            0: 'off'
+          },
           "prettyName": "RIMS Heater"
         }
       ],
@@ -27,51 +39,63 @@ var procedureBuilder = new Vue({
           "type": "divert",
           "name": "hltDivert",
           "location": 1,
+          "locationVerbage": {
+            1: 'mash',
+            0: 'boil'
+          },
           "prettyName": "HLT Divert Valve"
         },
         {
           "type": "divert",
           "name": "rimsDivert",
           "location": 1,
+          "locationVerbage": {
+            1: 'mash',
+            0: 'boil'
+          },
           "prettyName": "RIMS Divert Valve"
         }
       ]
+      // methods: [
+      //   {
+      //     "name": "sleep",
+      //     "prettyName": "Wait",
+      //     "args": [],
+      //     "arg_types": ["int", "float"],
+      //   },
+      //   {
+      //     "name": "slack",
+      //     "prettyName": "Slack",
+      //     "args": [],
+      //     "arg_types": ['str']
+      //   }
+      // ]
     },
 
     tableData: [
-      {
-        "type": "binary",
-        "name": "hlt",
-        "state": 0,
-        "prettyName": "HLT Valve"
-      },
-      {
-        "type": "divert",
-        "name": "rims",
-        "location": 1,
-        "prettyName": "RIMS Divert Valve"
-      },
-      {
-        "type": "binary",
-        "name": "pump",
-        "state": 0,
-        "prettyName": "Pump"
-      },
+
     ]
   },
 
   methods: {
     addSelected(id) {
-      this.relays['binaries'].forEach(function(relay) {
+      this.items['binaries'].forEach(function (relay) {
         if (relay.name == id) {
           this.tableData.push(relay)
         }
       }, this);
-      this.relays['diverts'].forEach(function(relay) {
+      this.items['diverts'].forEach(function (relay) {
         if (relay.name == id) {
           this.tableData.push(relay)
         }
       }, this);
+    },
+
+    removeElement(item) {
+      index = this.tableData.indexOf(item);
+      if (index > -1) {
+        this.tableData.splice(index, 1);
+      }
     }
   }
 
