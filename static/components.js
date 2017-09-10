5bf61dab2fae055b8c6b40a691cc8fb0a88f02a0
Luke Sweeney <luke@thesweeneys.org>
2017-09-10 17:57:09 -0500

    Add row and remove button to vue component

--- static/components.js
+++ static/components.js
@@ -36,3 +36,46 @@ Vue.component("panel-item", {
   `
 })
 
+
+
+Vue.component("item-row", {
+  props: ["item"],
+  template: `
+  <div>
+    <div v-if="item.type == 'binary'" class="select is-small">
+      <select v-model="item.state" name="state-select" id="state-select">
+        <option value="1">{{ item.stateVerbage[1] }}</option>
+        <option value="0">{{ item.stateVerbage[0] }}</option>
+      </select>
+    </div>
+    <div v-if="item.type == 'divert'" class="select is-small">
+      <select v-model="item.location" name="location-select" id="location-select">
+        <option value="1">mash</option>
+        <option value="0">boil</option>
+      </select>
+    </div>
+    <div v-if="item.type == 'method'">
+      <div class="field" v-for="arg in item.args">
+        <div class="control">
+          <input class="input" v-model="item.value" :name="arg" type="text" :placeholder="item.hrType">
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+})
+
+
+Vue.component('remove-button', {
+  methods: {
+    removeElement() {
+      this.$emit("remove");
+    }
+  },
+
+  template: `
+  <a @click="removeElement">
+    <i class="fa fa-remove fa-lg" aria-hidden="true"></i>
+  </a>
+  `
+})
